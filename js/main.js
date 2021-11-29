d59b48697b7e5bb90ae54ef91a70ac2e5f487668
Colin Dean <colindean@users.noreply.github.com>
2021-11-29 00:58:37 -0500

    Try catch on form attachment errors

--- js/main.js
+++ js/main.js
@@ -19,43 +19,48 @@ function initializeSurvey(){
     }
   };
 
-  form.elements.name.addEventListener(eventHook, function(event){
-    changeText('name-response', event.target.value);
-  });
-  form.elements['street-address'].addEventListener(eventHook, function(event){
-    changeText('address-response', event.target.value);
-    var lookup_link = document.getElementById('lookup-link');
-    lookup_link.setAttribute("href", lookup_link.getAttribute("data-base-url") + "?q=" + event.target.value.replace("\n"," "));
-  });
-  form.elements.voting_location.addEventListener(eventHook, function(event){
-    changeText('voting_location-response', event.target.value);
-  });
-  form.elements.wait_time.addEventListener(eventHook, function(event){
-    changeText('wait_time-response', event.target.value);
-  });
-  form.elements.acceptable.addEventListener(eventHook, function(event){
-    changeText('acceptable-response', event.target.value);
-  });
-  form.elements.intimidation_details.addEventListener(eventHook, function(event){
-    changeText('intimidation_details-response', event.target.value);
-  });
-  form.elements.blocked_details.addEventListener(eventHook, function(event){
-    changeText('blocked_details-response', event.target.value);
-  });
-  form.elements.comments.addEventListener(eventHook, function(event){
-    if(event.target.value.length > 0){
-      document.getElementById('comments').className = 'comments';
-    } else {
-      document.getElementById('comments').className = 'not-included';
-    }
-    changeText('comments', event.target.value);
-  });
-  form.elements.intimidation.addEventListener(eventHook, function(event){
-    toggleNotIncluded("intimidation", event.target.value);
-  });
-  form.elements.blocked.addEventListener(eventHook, function(event){
-    toggleNotIncluded("blocked", event.target.value);
-  });
+  try {
+
+    form.elements.name.addEventListener(eventHook, function(event){
+      changeText('name-response', event.target.value);
+    });
+    form.elements['street-address'].addEventListener(eventHook, function(event){
+      changeText('address-response', event.target.value);
+      var lookup_link = document.getElementById('lookup-link');
+      lookup_link.setAttribute("href", lookup_link.getAttribute("data-base-url") + "?q=" + event.target.value.replace("\n"," "));
+    });
+    form.elements.voting_location.addEventListener(eventHook, function(event){
+      changeText('voting_location-response', event.target.value);
+    });
+    form.elements.wait_time.addEventListener(eventHook, function(event){
+      changeText('wait_time-response', event.target.value);
+    });
+    form.elements.acceptable.addEventListener(eventHook, function(event){
+      changeText('acceptable-response', event.target.value);
+    });
+    form.elements.intimidation_details.addEventListener(eventHook, function(event){
+      changeText('intimidation_details-response', event.target.value);
+    });
+    form.elements.blocked_details.addEventListener(eventHook, function(event){
+      changeText('blocked_details-response', event.target.value);
+    });
+    form.elements.comments.addEventListener(eventHook, function(event){
+      if(event.target.value.length > 0){
+        document.getElementById('comments').className = 'comments';
+      } else {
+        document.getElementById('comments').className = 'not-included';
+      }
+      changeText('comments', event.target.value);
+    });
+    form.elements.intimidation.addEventListener(eventHook, function(event){
+      toggleNotIncluded("intimidation", event.target.value);
+    });
+    form.elements.blocked.addEventListener(eventHook, function(event){
+      toggleNotIncluded("blocked", event.target.value);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 if (window.addEventListener) { // Mozilla, Netscape, Firefox
